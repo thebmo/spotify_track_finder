8a0227619f0b98258e7b9514ac9ead1aede96da5
Brian Mosier <bmosier@gmail.com>
2019-05-23 17:42:03 -0400

    add helper methods to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,17 @@
 class User < ApplicationRecord
+  attr_reader :spotify_user
 
-   
+
+  def spotify_hash=(spotify_user)
+    spu_json = spotify_user.to_hash.to_json
+    super(spu_json)
+  end
+
+  def spotify_hash
+    JSON.parse(self[:spotify_hash])
+  end
+
+  def spotify_user
+    @spotify_user ||= RSpotify::User.new(spotify_hash)
+  end
 end
