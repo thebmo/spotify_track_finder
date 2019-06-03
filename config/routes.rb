2089991560b52365ddc005b8e9fcc0aa1f3ff3a1
Brian Mosier <bmosier@gmail.com>
2019-06-03 17:17:08 -0400

    new sessions methods

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/users/:id', to: 'test#show'
   get '/test', to: 'test#test', as: 'users_test'
   get '/spotify/sign_in', to: 'spotify#sign_in', as: 'spotify_sign_in'
   get '/auth/spotify/callback', to: 'spotify#callback', as: 'auth_spotify_callback'
   get '/band_tracks', to: 'spotify#band_tracks', as: 'band_tracks'
+
+  #sessions controller
+  get    '/login/', to: 'sessions#new'
+  post   '/login',  to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
 end
