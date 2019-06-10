9cfad8c6ffe8eabb708f9439422956f705997d04
Brian Mosier <bmosier@gmail.com>
2019-06-10 17:16:23 -0400

    move current_user to sessions helper

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,4 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include SessionsHelper
-
-  def current_user
-    return unless session[:user_id]
-    @current_user ||= User.find(session[:user_id])
-  end
-  helper_method :current_user
 end
