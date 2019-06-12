e3f752015f62ea619b3b3191bcf675d7102a3e8d
Brian Mosier <bmosier@gmail.com>
2019-06-12 14:21:53 -0400

    user scaffolding

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'users/new'
+  get 'users/create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/users/:id', to: 'test#show'
   get '/test', to: 'test#test', as: 'users_test'
