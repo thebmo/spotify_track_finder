23ec6ace4b2756d262a6e569b25ad5ceb03f2ba1
Brian Mosier <bmosier@gmail.com>
2019-06-24 15:09:05 -0400

    wrap spotify methods in a retry helper

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  include Spotify::Spotify
+
   attr_reader :spotify_user
 
   def initialize(*args)
@@ -32,20 +34,43 @@ class User < ApplicationRecord
     BCrypt::Engine.hash_secret(given_password, self.salt) == self.password
   end
 
-  # INTERFACE
-  def playlists(limt: 20, offset: 0)
-    spotify_user.playlists(limit: limit, offeset: offset)
+  # INTERFACE METHODS
+
+  def playlists(limit: 20, offset: 0)
+    retry_for_user(:playlists, [{ limit: limit, offset: offset }])
   end
 
   def create_playlist!(name, public: true)
-    spotify_user.create_playist(name, public: public)
+    retry_for_user(:create_playlist!, [name, { public: true }])
   end
 
   def recently_played(limit: 20)
-    spotify_user.recently_played(limit: limit)
+    retry_for_user(:recently_played, [{ limit: limit }])
   end
 
   def saved_tracks(limit: 20, offset: 0)
-    spotify_user.saved_tracks(limit: limit, offset: offset)
+    retry_for_user(:saved_tracks, [{ limit: limit, offset: offset }])
+  end
+
+  private
+
+  def retry_for_user(method, args)
+    i = 0
+    begin
+      spotify_user.send(method, *args)
+    rescue => e
+      if e.message.match?(/bad request/i) && i < 3
+        i+=1
+
+        re_auth_user!(self)
+
+        @spotify_user = nil
+        self.reload
+
+        retry
+      end
+
+      raise e
+    end
   end
 end
