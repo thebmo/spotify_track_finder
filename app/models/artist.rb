93ff361d2c8b5798832d34a79781098d3ab174ba
Brian Mosier <bmosier@gmail.com>
2019-06-30 21:34:11 -0400

    comments for the artist model

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,5 +1,6 @@
 class Artist < ApplicationRecord
 
+  # @param spotify_artist [RSpotify::Artist] an artist object
   def initialize(spotify_artist)
     remote_id = spotify_artist.id
     name = spotify_artist.name
@@ -7,10 +8,14 @@ class Artist < ApplicationRecord
     super(remote_id: remote_id, name: name, artist_hash: artist_hash)
   end
 
+  # @returns an [RSpotify::Artist] object
   def spotify_artist
     @spotify_artist ||= dejsonify_artist
   end
 
+  # serializes the spotify artist object as json string
+  #
+  # @param spotify_artist [RSpotify::Artist] an artist object
   def jsonify_artist(spotify_artist)
     h = {}
     spotify_artist.instance_variables.each do |var|
@@ -20,6 +25,7 @@ class Artist < ApplicationRecord
     h.to_json
   end
 
+  # @return [RSpotify::Artist] from the models cache
   def dejsonify_artist
     artist = RSpotify::Artist.new()
     JSON.parse(self.artist_hash).each do |k, v|
