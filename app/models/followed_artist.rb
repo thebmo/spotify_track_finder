f16eb6ba529d54b7c61dcf40b6da3b3b8b4fa5bd
Brian Mosier <bmosier@gmail.com>
2019-07-09 17:31:16 -0400

    add validation and testing

--- app/models/followed_artist.rb
+++ app/models/followed_artist.rb
@@ -1,14 +1,15 @@
 class FollowedArtist < ApplicationRecord
+  validates :artist_remote_id, :user_id, :artist_name, :artist_id, presence: true
 
   # @param artist [Artist] an artist object
   # @param user [User] a user object
-  def initialize(artist, user)
-    validate_argument!(artist, Artist)
-    validate_argument!(user, User)
+  def self.create_from_objects(artist, user)
+    self.validate_argument!(artist, "Artist")
+    self.validate_argument!(user, "User")
 
-    super(
+    FollowedArtist.create(
       artist_remote_id: artist.remote_id,
-      user_id: user.id
+      user_id: user.id,
       artist_name: artist.name,
       artist_id: artist.id)
   end
@@ -18,8 +19,8 @@ class FollowedArtist < ApplicationRecord
   # Raises an ArgumentError if not a RSpotify::Artist object
   #
   # @param argument [Object] an artist object or a user object
-  def validate_argument!(argument, klass)
-    if !argument || !argument.is_a?(klass)
+  def self.validate_argument!(argument, klass)
+    if !argument || !argument.class.to_s == klass
       raise ArgumentError.new("#{argument.class} given but #{klass} is needed.")
     end
   end
